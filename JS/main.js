6ab5fea3c8f5c78f041b86d8c99a04c0ba8e623a
Eman-AbdElZaher <e.m.abdelzaher@gmail.com>
2023-06-01 23:26:43 +0200

    complete validation of form

--- JS/main.js
+++ JS/main.js
@@ -9,13 +9,25 @@ const cardMonthInput= document.getElementById('month');
 const cardYearInput = document.getElementById('year');
 const cardCvcInput = document.getElementById('cvc');
 const confirmButton = document.getElementById('confirm');
+const form = document.querySelector('form');
+const completedSubmission = document.querySelector('.completed');
 
 
 const GetInputValue = (e,element) =>  element.textContent = e.target.value;
 
 
 cardNameInput.addEventListener('input',(e) =>{GetInputValue(e,cardOwnerName)});
-cardNumberInput.addEventListener('input',(e) =>{GetInputValue(e,cardNumber)});
+
+cardNumberInput.addEventListener('input',(e) => {
+
+    const value = e.target.value;
+    const words = [];
+
+    for (let i = 0; i < value.length; i += 4) {
+      words.push(value.substr(i, 4));
+    }
+    cardNumber.textContent= words.join(' ');
+});
 cardMonthInput.addEventListener('input',(e) =>{GetInputValue(e,cardExpirationDateMonth)});
 cardYearInput.addEventListener('input',(e) =>{GetInputValue(e,cardExpirationDateYear)});
 cardCvcInput.addEventListener('input',(e) => {GetInputValue(e,cardPin)});
@@ -39,11 +51,9 @@ const isValid = (input) => {
 const isNotValid = (input,message) => {
 
     const formField = input.parentElement;
-
+    input.classList.add('invalid');
     const errorText = formField.querySelector('small');
     errorText.textContent = message;
-    input.classList.add('invalid');
-
 }
 
 const validateInput = (input, regex, errorMessage) => {
@@ -63,19 +73,31 @@ const validateInput = (input, regex, errorMessage) => {
     return true;
   };
   
-const checkCardNameValidation = () => {
-    validateInput(cardNameInput,/^[a-zA-Z]{6} ?$/,'must be 6 length');
-}
+const checkCardNameValidation = () => validateInput(cardNameInput,/^[a-zA-Z]{6,20} ?$/,'must be 6 length');
 
-const checkCardNumber = () => {
-    validateInput(cardNumberInput,/^[0-9]{16}$/,'wrong format Numbers only');
-}
+const checkCardNumber = () => validateInput(cardNumberInput,/^[0-9]{16}$/,'wrong format Numbers only');
 
-const checkCvcValidation = () => {
- validateInput(cardCvcInput,/\d{3}/,'must be 3 numbers');
-}
+const checkCvcValidation = () => validateInput(cardCvcInput,/\d{3}/,'must be 3 numbers');
 
-document.querySelector('form').addEventListener('input',(e) => {
+const checkExpiryDateMonth = () => validateInput(cardMonthInput,/^[01][0-9]$/,'wrong Format');
+
+const checkExpiryDateyear = () => validateInput(cardYearInput,/^(2[3-9])$/,'wrong Format');
+
+
+const debounce = (fn, delay = 500) => {
+    let timeoutId;
+    return (...args) => {
+
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+
+        timeoutId = setTimeout(() => {
+            fn.apply(null, args)
+        }, delay);
+    };
+};
+document.querySelector('form').addEventListener('input',debounce((e) => {
     switch (e.target.id) {
 
         case 'card-name':
@@ -87,5 +109,50 @@ document.querySelector('form').addEventListener('input',(e) => {
         case 'cvc':
             checkCvcValidation();
             break;
+        case 'month':
+            checkExpiryDateMonth();
+            break;
+        case 'year':
+            checkExpiryDateyear();
+            break;
     }
-})
+}));
+
+form.addEventListener('submit',  (e) => {
+    
+    e.preventDefault();
+
+    let iscardNameValid = checkCardNameValidation(),
+        isCardNumberValid = checkCardNumber(),
+        isExpiryMonthValid = checkExpiryDateMonth(),
+        isExpiryYearValid = checkExpiryDateyear(),
+        isPinNumberValid = checkCvcValidation();
+
+    const isFormValid = iscardNameValid&&
+                        isCardNumberValid&&
+                        isExpiryMonthValid&&
+                        isExpiryYearValid&&
+                        isPinNumberValid;
+
+    if(isFormValid) {
+      form.style.display='none';
+      completedSubmission.style.display='block';
+    }
+});
+
+const clearFormInputs = () => {
+
+ cardNameInput.value='';
+ cardNumberInput.value='';
+ cardMonthInput.value='';
+ cardYearInput.value='';
+ cardCvcInput.value='';
+
+};
+
+document.getElementById('continue').addEventListener('click',() => {
+
+    completedSubmission.style.display='none';
+    form.style.display='block';
+    clearFormInputs();
+});
